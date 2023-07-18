ef1269233eedbcfa627bc870c4be5ffa3edd5fd7
Anirudhj9 <55417765+Anirudhj9@users.noreply.github.com>
2023-07-18 11:42:05 +0530

    7 files modified

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-aboutpage',
   templateUrl: './about.component.html',
   styleUrls: ['about.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppAboutPage {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'home', canActivate: [AuthGuard], component: AppHomePage },
   { path: 'login', component: AppLoginPage },
   { path: 'register', component: AppRegisterPage },
-  { path: 'about', canActivate: [AuthGuard], component: AppAboutPage },
+  { path: 'about', component: AppAboutPage },
 ];
 export const routerProviders = [provideRouter(routes)];
 
